20fb4ebe71a7d3acacf7baf457f8f5720d048580
AmanRoy <72335987+amanroy2001@users.noreply.github.com>
2021-06-26 20:20:58 +0530

    Add files via upload

---
+++ oddevencheck.java
@@ -0,0 +1,16 @@
+package cwa;
+import java.util.Scanner;
+public class oddevencheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc= new Scanner(System.in);
+System.out.println("plz enter a number");
+int a=sc.nextInt();
+if(a%2==0)
+System.out.println("number is even");
+else
+	System.out.println("number is odd");
+	}
+
+}
